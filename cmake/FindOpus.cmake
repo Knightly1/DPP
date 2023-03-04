f2ae9364f2ba97240273e5746753507d6e8afc55
Knightly <knightly.cavalier@gmail.com>
2023-03-03 16:53:15 -1000

    test

--- cmake/FindOpus.cmake
+++ cmake/FindOpus.cmake
@@ -2,29 +2,36 @@
 #  OPUS_INCLUDE_DIRS - the opus include directory
 #  OPUS_LIBRARIES - The libraries needed to use opus
 
-find_path(OPUS_INCLUDE_DIRS
-	NAMES opus/opus.h
-	PATH_SUFFIXES include
-)
+if(WIN32)
+	if(OPUS_USE_STATIC_LIBS)
+		find_package(opus CONFIG REQUIRED STATIC)
+	else()
+		find_package(opus CONFIG REQUIRED)
+	endif()
+else()
+	find_path(OPUS_INCLUDE_DIRS
+		NAMES opus/opus.h
+		PATH_SUFFIXES include
+	)
+	if(OPUS_USE_STATIC_LIBS)
+		find_library(OPUS_LIBRARIES NAMES "libopus.a")
+	else()
+		find_library(OPUS_LIBRARIES NAMES opus)
+	endif()
+	if(OPUS_LIBRARIES)
+		if(OPUS_USE_STATIC_LIBS)
+			find_library(LIBM NAMES "libm.a" "libm.tbd")
+		else()
+			find_library(LIBM NAMES m)
+		endif()
+		if(LIBM)
+			list(APPEND OPUS_LIBRARIES ${LIBM})
+		endif()
+	endif()
+endif()
+
 if(OPUS_INCLUDE_DIRS)
 	set(HAVE_OPUS_OPUS_H 1)
-endif()
-
-if(OPUS_USE_STATIC_LIBS)
-	find_library(OPUS_LIBRARIES NAMES "libopus.a")
-else()
-	find_library(OPUS_LIBRARIES NAMES opus)
-endif()
-
-if(OPUS_LIBRARIES)
-	if(OPUS_USE_STATIC_LIBS)
-		find_library(LIBM NAMES "libm.a" "libm.tbd")
-	else()
-		find_library(LIBM NAMES m)
-	endif()
-	if(LIBM)
-		list(APPEND OPUS_LIBRARIES ${LIBM})
-	endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
